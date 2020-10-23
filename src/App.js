3a374d58ffe6edccf23120a2eb8f58d01b618c2e
Clément van Staen <clement.vanstaen@gmail.com>
2020-10-24 01:03:58 +0200

    first draft of Auth page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import AuthPage from "./pages/Auth";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the Rewaer Frontend</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/looks" component={null} />
+        <Route path="/items" component={null} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 

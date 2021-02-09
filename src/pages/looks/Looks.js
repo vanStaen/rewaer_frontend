0338f43b9eba07317c2a7729d8af25d26e04bd40
Clément van Staen <clement.vanstaen@gmail.com>
2021-02-09 19:48:20 +0100

    looks refactoring

--- src/pages/looks/Looks.js
+++ src/pages/looks/Looks.js
@@ -1,72 +1,42 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Redirect } from "react-router-dom";
-import axios from 'axios';
 import { Col, Row, Spin, notification } from "antd";
+
+import fetchLooks from './fetchLooks';
 
 import LookCard from "./LookCard/LookCard";
 import LookForm from "./LookForm/LookForm";
-import AuthContext from "../../context/auth-context";
 
 import "./Looks.css";
 
 const LooksPage = () => {
 
   const [looks, setLooks] = useState([]);
-  const [isOutOfDate, setIsOutOfDate] = useState(false);
+  const [isOutOfDate, setIsOutOfDate] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const context = useContext(AuthContext);
-
   useEffect(() => {
-    loadLooks();
+    LoadLooks();
   }, [isOutOfDate])
 
-  const loadLooks = () => {
-    const requestBody = {
-      query: `
-            query {
-                looks {
-                  _id
-                  title
-                  active
-                  favorite
-                  mediaUrlThumb
-                }
-              }
-              `,
-    };
-
-    async function fetchLooks() {
-      const response = await axios({
-        url: process.env.REACT_APP_API_URL,
-        method: "POST",
-        data: requestBody,
-      });
-      if ((response.status !== 200) & (response.status !== 201)) {
-        throw new Error("Unauthenticated!");
-      }
-      const looks = await response.data;
-      return looks;
-    }
-    // fetch Looks
-    fetchLooks().then((resData) => {
-      const looks = resData.data.looks;
+  const LoadLooks = async () => {  
+    try {
+      const looks = await fetchLooks();
       setLooks(looks);
       setIsLoading(false);
       setIsOutOfDate(false);
+    } catch (error) {
+        console.log(error.message);
+        notification.error({
+          message: "Error",
+          description: error.message,
+          placement: "bottomRight",
+        });
+        setError(error.message);
     }
-    ).catch(error => {
-      console.log(error.message);
-      notification.error({
-        message: "Error",
-        description: error.message,
-        placement: "bottomRight",
-      });
-      setError(error.message);
-    });
-  };
-
+  }
+  
   const lookList = looks.map(look => {
     return (<Col key={look._id}>
       <LookCard

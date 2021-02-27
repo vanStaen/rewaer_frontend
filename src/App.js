bac1f4a4e4a0f1cd3f1993f3d5cfbf7e6bc2a315
vanStaen <clement.vanstaen@gmail.com>
2021-02-27 15:24:38 +0100

    refactor app to function component + mobX observer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import { authStore } from './stores/authStore';
+import { observer } from "mobx-react";
 import { openNotification } from "./components/openNotification/openNotification";
 import axios from 'axios';
 
@@ -15,9 +16,9 @@ import MenuBar from "./components/MenuBar/MenuBar";
 
 import "./App.css";
 
-class App extends Component {
+const App = observer(() => {
 
-  dummyCall() {
+  const dummyCall = () => {
 
     // call the the dummy endpoint to wake the backend.
     let requestBody = {
@@ -46,12 +47,12 @@ class App extends Component {
       });
   }
 
-  componentDidMount() {
+  useEffect(() => {
 
     console.log("authStore.refreshToken", authStore.refreshToken);
     console.log("authStore.token", authStore.token);
 
-    this.dummyCall();
+    dummyCall();
 
     // Axios Interceptors
     axios.interceptors.request.use(async (config) => {
@@ -69,66 +70,64 @@ class App extends Component {
     }, (error) => {
       return Promise.reject(error);
     });
-  }
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <React.Fragment>
-            <MenuBar />
-            <main className="main-content">
-              <Switch>
-                {!authStore.refreshToken && (
-                  <Redirect from="/" to="/auth" exact />
-                )}
-                {!authStore.refreshToken && (
-                  <Route path="/auth" component={AuthPage} />
-                )}
-                {!authStore.refreshToken && (
-                  <Redirect from="/looks" to="/auth" exact />
-                )}
-                {!authStore.refreshToken && (
-                  <Redirect from="/items" to="/auth" exact />
-                )}
-                {!authStore.refreshToken && (
-                  <Redirect from="/mail" to="/auth" exact />
-                )}
-                {!authStore.refreshToken && (
-                  <Redirect from="/friends" to="/auth" exact />
-                )}
-                {!authStore.refreshToken && (
-                  <Redirect from="/profile" to="/auth" exact />
-                )}
-                {authStore.refreshToken && (
-                  <Redirect from="/" to="/profile" exact />
-                )}
-                {authStore.refreshToken && (
-                  <Redirect from="/auth" to="/profile" exact />
-                )}
-                {authStore.refreshToken && (
-                  <Route path="/looks" component={LooksPage} />
-                )}
-                {authStore.refreshToken && (
-                  <Route path="/items" component={ItemsPage} />
-                )}
-                {authStore.refreshToken && (
-                  <Route path="/mail" component={MailPage} />
-                )}
-                {authStore.refreshToken && (
-                  <Route path="/friends" component={FriendsPage} />
-                )}
-                {authStore.refreshToken && (
-                  <Route path="/profile" component={ProfilPage} />
-                )}
-                <Route path="/info" component={InfoPage} />
-              </Switch>
-            </main>
-          </React.Fragment>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <React.Fragment>
+          <MenuBar />
+          <main className="main-content">
+            <Switch>
+              {!authStore.refreshToken && (
+                <Redirect from="/" to="/auth" exact />
+              )}
+              {!authStore.refreshToken && (
+                <Route path="/auth" component={AuthPage} />
+              )}
+              {!authStore.refreshToken && (
+                <Redirect from="/looks" to="/auth" exact />
+              )}
+              {!authStore.refreshToken && (
+                <Redirect from="/items" to="/auth" exact />
+              )}
+              {!authStore.refreshToken && (
+                <Redirect from="/mail" to="/auth" exact />
+              )}
+              {!authStore.refreshToken && (
+                <Redirect from="/friends" to="/auth" exact />
+              )}
+              {!authStore.refreshToken && (
+                <Redirect from="/profile" to="/auth" exact />
+              )}
+              {authStore.refreshToken && (
+                <Redirect from="/" to="/profile" exact />
+              )}
+              {authStore.refreshToken && (
+                <Redirect from="/auth" to="/profile" exact />
+              )}
+              {authStore.refreshToken && (
+                <Route path="/looks" component={LooksPage} />
+              )}
+              {authStore.refreshToken && (
+                <Route path="/items" component={ItemsPage} />
+              )}
+              {authStore.refreshToken && (
+                <Route path="/mail" component={MailPage} />
+              )}
+              {authStore.refreshToken && (
+                <Route path="/friends" component={FriendsPage} />
+              )}
+              {authStore.refreshToken && (
+                <Route path="/profile" component={ProfilPage} />
+              )}
+              <Route path="/info" component={InfoPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    </div>
+  );
+});
 
 export default App;

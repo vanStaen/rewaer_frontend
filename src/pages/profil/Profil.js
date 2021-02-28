c8c743f9224a37275034617b8845d60fe6aa3b5a
vanStaen <clement.vanstaen@gmail.com>
2021-02-28 13:42:01 +0100

    user store mobX

--- src/pages/profil/Profil.js
+++ src/pages/profil/Profil.js
@@ -1,48 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
+import { observer } from "mobx-react";
 import { authStore } from '../../stores/authStore';
+import { userStore } from '../../stores/userStore';
 import { Typography } from "antd";
+
 const { Title, Paragraph } = Typography;
 
+const ProfilPage = observer(() => {
 
-class ProfilPage extends Component {
+  return (
+    <div>
+      <Title level={3}>Hello, {userStore.user ? userStore.user.name : 'there'}
+      </Title>
+      <Paragraph copyable>
+        <b>User ID:</b> {userStore.userId}
+      </Paragraph>
+      { process.env.NODE_ENV === "development" && (
+        <div>
 
-  render() {
+          <Paragraph
+            copyable={{
+              text: authStore.token,
+              tooltips: ["Copy token", "Token copied!"],
+            }}
+          >
+            <b>Access Token : </b>{authStore.token}
+          </Paragraph>
+          <Paragraph
+            copyable={{
+              text: authStore.refreshToken,
+              tooltips: ["Copy refresh token", "Refresh Token copied!"],
+            }}
+          >
+            <b>Refresh Token : </b>{authStore.refreshToken}
+          </Paragraph>
+        </div>
+      )}
 
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    const userId = localStorage.getItem("userId");
-
-    return (
-      <div>
-        <Title level={3}>Hello, {storedUser ? storedUser.name : 'there'}
-        </Title>
-        <Paragraph copyable>
-          <b>User ID:</b> {userId}
-        </Paragraph>
-        { process.env.NODE_ENV === "development" && (
-          <div>
-
-            <Paragraph
-              copyable={{
-                text: authStore.token,
-                tooltips: ["Copy token", "Token copied!"],
-              }}
-            >
-              <b>Access Token : </b>{authStore.token}
-            </Paragraph>
-            <Paragraph
-              copyable={{
-                text: authStore.refreshToken,
-                tooltips: ["Copy refresh token", "Refresh Token copied!"],
-              }}
-            >
-              <b>Refresh Token : </b>{authStore.refreshToken}
-            </Paragraph>
-          </div>
-        )}
-
-      </div>
-    );
-  }
-}
+    </div>
+  );
+});
 
 export default ProfilPage;
---
+++ src/stores/userStore.js
@@ -0,0 +1,17 @@
+import { makeObservable, observable } from "mobx";
+
+export class userStoreImplementation {
+
+    user = JSON.parse(localStorage.getItem('user'));
+    userId = localStorage.getItem('userId');
+
+    constructor() {
+        makeObservable(this, {
+            user: observable,
+            userId: observable,
+        });
+    }
+
+}
+
+export const userStore = new userStoreImplementation();
